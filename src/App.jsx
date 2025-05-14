f6e5f491be61ca1922754b4e307fba63a56b1696
NguyenLoc <loc7102010@gmail.com>
2025-05-14 14:13:23 +0700

    #37

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
 import TodoTitle from "./components/todo/TodoTitle";
 import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 function App() {
+  const [todoList, setTodoList] = useState([
+    {
+      id: 1,
+      name: "Learning React"
+    }, {
+      id: 2,
+      name: "Youtube React"
+    }
+  ])
+
+
   const hoidanit = "LOC";
   const age = 25;
   const data = {
@@ -16,13 +28,14 @@ function App() {
   return (
     <div className="todo-container">
       <TodoTitle />
-      <TodoNew 
-      addNewTodo={addNewTodo}
+      <TodoNew
+        addNewTodo={addNewTodo}
       />
-      <TodoData 
-      name = {hoidanit}
-      age = {age}
-      data = {data}
+      <TodoData
+        name={hoidanit}
+        age={age}
+        data={data}
+        todoList={todoList}
       />
       <div className="todo-image">
         <img className="logo" src={reactLogo} alt="" />
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 const TodoNew = (props) => {
-  console.log(props);
-  const {addNewTodo} = props
+
+  const [valueInput, setValueInput] = useState("LOC")
+
+  const { addNewTodo } = props
+
+  const handelClick = () => {
+    console.log(">>> handelClick ", valueInput);
+  }
+  const handelOnChange = (event) => {
+    setValueInput(event)
+  }
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input type="text"
+        onChange={(event) => handelOnChange(event.target.value)} />
+      <button style={{ cursor: "pointer" }} onClick={handelClick}>Add</button>
+      <div>My text input = {valueInput}</div>
     </div>
   );
 }
