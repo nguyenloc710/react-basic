ae3212f8681d21d9390e4e14ef718aa7061793df
nguyenloc710 <loc7102010@gmail.com>
2025-06-10 21:59:22 +0700

    #89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,29 @@
-const RegisterPage = () => {
-    return (
-        <div>Register Page</div>
+import { Button, Input } from "antd";
 
-    )
-}
+const RegisterPage = () => {
+  return (
+    <div style={{ display: "flex", gap: "15px", flexDirection: "column", margin: "50px" }}>
+      <div>
+        <span>FullName</span>
+        <Input />
+      </div>
+      <div>
+        <span>Email</span>
+        <Input />
+      </div>
+      <div>
+        <span>Password</span>
+        <Input.Password />
+      </div>
+      <div>
+        <span>Phone number</span>
+        <Input />
+      </div>
+      <div>
+        <Button type="primary">Register</Button>
+      </div>
+    </div>
+  );
+};
 
 export default RegisterPage;
